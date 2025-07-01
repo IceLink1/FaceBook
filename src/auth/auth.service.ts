2e51956ada9212393fecaccc76d6c84170426539
ICELINK <icelink>
2025-07-01 07:20:02 +0500

    make  merge to main branch

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,59 +1,60 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import {Injectable, UnauthorizedException} from '@nestjs/common';
+import {JwtService} from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
-import { UsersService } from '../users/users.service';
-import { LoginDto } from './dto/login.dto';
-import { CreateUserDto } from '../users/dto/create-user.dto';
+import {UsersService} from '../users/users.service';
+import {LoginDto} from './dto/login.dto';
+import {CreateUserDto} from '../users/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private usersService: UsersService,
-    private jwtService: JwtService,
-  ) {}
-
-  async validateUser(email: string, password: string): Promise<any> {
-    const user = await this.usersService.findByEmail(email);
-    if (user && await bcrypt.compare(password, user.password)) {
-      const { password, ...result } = user.toObject();
-      return result;
-    }
-    return null;
-  }
-
-  async login(loginDto: LoginDto) {
-    const user = await this.validateUser(loginDto.email, loginDto.password);
-    if (!user) {
-      throw new UnauthorizedException('Invalid credentials');
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) {
     }
 
-    const payload = { email: user.email, sub: user._id };
-    return {
-      access_token: this.jwtService.sign(payload),
-      user: {
-        id: user._id,
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        avatar: user.avatar,
-      },
-    };
-  }
+    async validateUser(email: string, password: string): Promise<any> {
+        const user = await this.usersService.findByEmail(email);
+        if (user && await bcrypt.compare(password, user.password)) {
+            const {password, ...result} = user.toObject();
+            return result;
+        }
+        return null;
+    }
 
-  async register(createUserDto: CreateUserDto) {
-    const user:any = await this.usersService.create(createUserDto);
-    
-    const payload = { email: user.email, sub: user._id };
-    
-    return {
-      access_token: this.jwtService.sign(payload),
-      user: {
-        id: user.id,
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        avatar: user.avatar,
-      },
-    };
-  }
+    async login(loginDto: LoginDto) {
+        const user = await this.validateUser(loginDto.email, loginDto.password);
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const payload = {email: user.email, sub: user._id};
+        return {
+            access_token: this.jwtService.sign(payload),
+            user: {
+                id: user._id,
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                avatar: user.avatar,
+            },
+        };
+    }
+
+    async register(createUserDto: CreateUserDto) {
+        const user: any = await this.usersService.create(createUserDto);
+
+        const payload = {email: user.email, sub: user._id};
+
+        return {
+            access_token: this.jwtService.sign(payload),
+            user: {
+                id: user.id,
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                avatar: user.avatar,
+            },
+        };
+    }
 }
